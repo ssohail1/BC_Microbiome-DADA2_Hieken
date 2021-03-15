fdb866b7bff15cf7c291223ad3f576b63fb89c98
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-03-15 10:29:53 -0500

    Update Starting_PCoA_Analysis.R
    Comment about input in distancematrix command

--- Hieken_files/PCoA/Starting_PCoA_Analysis.R
+++ Hieken_files/PCoA/Starting_PCoA_Analysis.R
@@ -2,6 +2,6 @@
 # updated Oct 26 2020
 BiocManager::install("hopach")
 #distmat<-distancematrix(seqtab.nochim, d = "euclid", na.rm=TRUE)
-distmat<-dist(seqtab.nochim, method = "euclidean", diag = FALSE, upper = FALSE, p = 2)
+distmat<-dist(seqtab.nochim, method = "euclidean", diag = FALSE, upper = FALSE, p = 2) #distance matrix with ASV table as input
 pc_plot<-pcoa(distmat)
 biplot(pc_plot)
